2249d34192991e38a93eaad3b2c457c8dfbcb651
paliwalishita <ishitapaliwal5600@gmail.com>
2024-02-14 19:49:22 +0530

    add initial files

--- todo.js
+++ todo.js
@@ -16,3 +16,103 @@ window.onload = render;
         <button class="delete">x</button>
        </li>` ;
     }
+    
+
+    
+    // display completed task from local storage
+    function completedTask(id){
+        console.log(id, itemsArray)
+        const index = itemsArray.findIndex(e => e.id === +id);
+
+        if (index !== -1) {
+        itemsArray[index].completed = !(itemsArray[index].completed)
+        localStorage.setItem("items", JSON.stringify(itemsArray));
+        }
+    
+ }
+
+     //keyup event filter li 
+    function filterTodos(term){
+    Array.from(task.children)
+    .filter( item => !item.textContent.includes(term))
+    .forEach( item => item.classList.add('filtered'));
+
+    Array.from(task.children)
+    .filter( item => item.textContent.includes(term))
+    .forEach( item => item.classList.remove('filtered'));
+}
+
+     // add items to local storage
+     function createItemList(todo) {
+        let dataObj = {
+        item: todo,
+        id: counter,
+        completed: false
+    }
+    
+    itemsArray.push(dataObj);
+    localStorage.setItem("items", JSON.stringify(itemsArray));
+    localStorage.setItem("counter", JSON.stringify(++counter));
+    render();
+}
+
+//get data from local RENDER function
+     function render() {
+    let data = '';
+
+    if (itemsArray) {
+        for (item of itemsArray) {
+            data += todoTemplate(item);
+        }
+        task.innerHTML = data;
+    }
+
+}
+
+// delete items from local storage
+    function deleteListItem(i){
+    itemsArray.splice(i, 1);
+    localStorage.setItem("items", JSON.stringify(itemsArray));
+    render();
+}
+
+    submitTask.addEventListener('click', e => {
+        e.preventDefault();
+        let todo = addTask.value.trim();
+        console.log(todo)
+        addTask.value = '';
+        if (todo == '') {
+            alert("Please enter a task");
+        }
+     else {
+            task.innerHTML += todoTemplate(todo);
+            createItemList(todo);
+        }
+    
+    }
+);
+
+
+//delete item from list
+task.addEventListener('click', e => {
+    if (e.target.classList.contains('delete')) {
+        const index = Array.from(e.target.parentElement).indexOf(e.target.parentElement) - 1;
+        deleteListItem(index);
+        e.target.parentElement.remove();
+    }
+
+});
+// toggle complete class
+task.addEventListener('click', e => {
+
+    if (e.target.tagName === 'LI') {
+         e.target.classList.toggle('complete');
+        completedTask(e.target.id);
+    }
+
+});
+//filtered list
+addTask.addEventListener('keyup', e => {
+    let term = addTask.value.trim();
+    filterTodos(term);
+});
